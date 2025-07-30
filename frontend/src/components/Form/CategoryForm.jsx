7c5e6313d6a902c7d41f1b6e69d640ae6ab30ec2
Namuna <namunasharma1234@gmail.com>
2025-07-30 20:34:46 +0545

    Fix: Database issue

--- frontend/src/components/Form/CategoryForm.jsx
+++ frontend/src/components/Form/CategoryForm.jsx
@@ -3,6 +3,15 @@ import React from "react";
 const CategoryForm = ({ handleSubmit, value, setValue }) => {
   return (
     <>
+      <div className="mb-3">
+        <input
+          type="text"
+          className="w-full px-4 py-2 text-left border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-500"
+          placeholder="Enter new category"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </div>
       <form onSubmit={handleSubmit} className="max-w-sm mx-auto">
         <button
           type="submit"
