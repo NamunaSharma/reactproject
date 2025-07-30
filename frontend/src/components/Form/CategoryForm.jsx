25fbb585c59bb02d4334fc8c7a7a0d541923811b
Namuna <namunasharma1234@gmail.com>
2025-07-30 20:33:36 +0545

    Fix: Button issues

--- frontend/src/components/Form/CategoryForm.jsx
+++ frontend/src/components/Form/CategoryForm.jsx
@@ -7,14 +7,19 @@ const CategoryForm = ({ handleSubmit, value, setValue }) => {
         <div className="mb-3">
           <input
             type="text"
-            className="border border-gray-300 rounded-md py-2 px-4 w-full focus:outline-none focus:ring focus:border-blue-500 text-left"
+            className="w-full px-4 py-2 text-left border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-500"
             placeholder="Enter new category"
             value={value}
             onChange={(e) => setValue(e.target.value)}
           />
         </div>
 
-        
+        <button
+          type="submit"
+          className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-500 "
+        >
+          Submit
+        </button>
       </form>
     </>
   );
